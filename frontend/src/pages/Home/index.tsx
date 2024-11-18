e452e229980d27b0f7a42ab3287ef9f65402cd29
magnitopic <magnitopic@gmail.com>
2024-11-18 07:41:40 +0100

    Landing page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import MainHeader from "./MainHeader";
 
 const index = () => {
 	return (
 		<main className="flex flex-1 justify-center items-center flex-col">
-			<h1>Home</h1>
+			<MainHeader />
 		</main>
 	);
 };
